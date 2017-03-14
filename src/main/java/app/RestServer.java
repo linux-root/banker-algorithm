57b348f3d7255c3d4473aa3fab30974267857884
Kurro <superusr.root@gmail.com>
2017-03-14 20:54:04 +0700

    send - receive data

--- src/main/java/app/RestServer.java
+++ src/main/java/app/RestServer.java
@@ -35,30 +35,13 @@ import java.util.List;
  */
 public class RestServer extends WebServer<AppConfig> {
 
-    private String CLIENT_ID = "306684131134-vlgl9ibaoo8fboo2o1dsrgv24h2ov648.apps.googleusercontent.com";
-    private HttpTransport transport;
-    private com.google.api.client.json.JsonFactory jsonFactory;
-    private final JsonObject JWTConfig = new JsonObject()
-            .put("keyStore", new JsonObject().put("path", "keystore.jceks").put("type", "jceks").put("password", "secret"));
-    private JWTAuth authProvider;
-    private Repository repository = new Repository();
     public RestServer(Vertx vertx, AppConfig config) {
         super(vertx, config);
-        authProvider = JWTAuth.create(vertx, JWTConfig);
     }
-
-    private final JWTOptions jwtOptions = new JWTOptions()
-            .setExpiresInMinutes(Long.parseLong("60"))
-            .addPermission("get")
-            .setAlgorithm("HS256");
 
     @Override
     protected void setupRouter(Router router) throws Exception {
         this.setupCorsHandler(router);
-        router.route("/v1/*").handler(JWTAuthHandler.create(authProvider, "/v1/authorization.json"));
-        router.post("/v1/authorization.json").handler(this::generateJWT);
-        router.put("/v1/authorization.json").handler(this::refreshJWT);
-        router.get("/v1/models.json").handler(this::getModels);
         router.post("/banker").handler(this::checkSafeStatus);
     }
 
@@ -69,63 +52,6 @@ public class RestServer extends WebServer<AppConfig> {
         JsonResponse<ArrayList> jsonResponse = new JsonResponse<>(context, 200);
         jsonResponse.setData(result);
         jsonResponse.write();
-    }
-
-    private void generateJWT(RoutingContext context) {
-        //verify
-        String idTokenString = context.request().getParam("gtoken"); //get google token from client's HTTPS POST
-        try {
-            transport = GoogleNetHttpTransport.newTrustedTransport();
-        } catch (GeneralSecurityException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        jsonFactory = new JacksonFactory();
-
-        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(transport, jsonFactory)
-                .setAudience(Collections.singletonList(CLIENT_ID))
-                .build();
-        GoogleIdToken idToken = null;
-        try {
-            idToken = verifier.verify(idTokenString);
-        } catch (GeneralSecurityException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        if (idToken != null) {
-            GoogleIdToken.Payload payload = idToken.getPayload();
-            // Print user identifier
-            String userId   = payload.getSubject();
-            String name     = (String) payload.get("name");
-            String email    = payload.getEmail();
-            String theToken = authProvider.generateToken(
-                    new JsonObject()
-                            .put("email", email)
-                            .put("name", name)
-                            .put("user_id", userId),
-                    jwtOptions);
-            Document data = new Document()
-                    .append("access_token", theToken)
-                    .append("expires", 60);
-            JsonResponse<Document> jsonResponse = new JsonResponse<>(context, 200);
-            jsonResponse.setData(data);
-            jsonResponse.write();
-        } else {
-            JsonResponse<Document> jsonResponse = new JsonResponse<>(context, 401);
-            jsonResponse.setData(new Document());
-            jsonResponse.write();
-        }
-    }
-
-    private void refreshJWT(RoutingContext context) {
-        // nhan refresh token
-        // check xem no da het han chua
-        // neu chua het han thi renew han moi
-        // neu het han roi thi fail. 401.
-
     }
 
 
@@ -148,53 +74,4 @@ public class RestServer extends WebServer<AppConfig> {
         jsonResponse.setData("everything is ok");
         jsonResponse.write();
     }
-
-    private void getModels(RoutingContext context) {
-        JsonResponse<List<Model>> jsonResponse = new JsonResponse<>(context, 200);
-        jsonResponse.setData(new ArrayList<>(repository.getModels().values()));
-        jsonResponse.write();
-    }
-
-    private void getModel(RoutingContext context) {
-        JsonRequest<Model> jsonRequest = JsonUtils.decode(context, Model.class);
-        String id = jsonRequest.getContext().request().getParam("id");
-
-        Model model = repository.getModel(id);
-
-        JsonResponse<Model> jsonResponse = new JsonResponse<>(context, 200);
-        jsonResponse.setData(model);
-        jsonResponse.write();
-    }
-
-    private void createModel(RoutingContext context) {
-        JsonRequest<Model> jsonRequest = JsonUtils.decode(context, Model.class);
-        Model createdModel = repository.create(jsonRequest.getData());
-
-        JsonResponse<Model> jsonResponse = new JsonResponse<>(context, 201);
-        jsonResponse.setData(createdModel);
-        jsonResponse.write();
-    }
-
-    private void updateModel(RoutingContext context) {
-        JsonRequest<Model> jsonRequest = JsonUtils.decode(context, Model.class);
-        String id = jsonRequest.getContext().request().getParam("id");
-
-        Model updatedModel = repository.update(id, jsonRequest.getData());
-
-        JsonResponse<Model> jsonResponse = new JsonResponse<>(context, 200);
-        jsonResponse.setData(updatedModel);
-        jsonResponse.write();
-    }
-
-    private void deleteModel(RoutingContext context) {
-        JsonRequest<Model> jsonRequest = JsonUtils.decode(context, Model.class);
-        String id = jsonRequest.getContext().request().getParam("id");
-
-        Model deletedModel = repository.delete(id);
-
-        JsonResponse<Model> jsonResponse = new JsonResponse<>(context, 200);
-        jsonResponse.setData(deletedModel);
-        jsonResponse.write();
-    }
-
 }
